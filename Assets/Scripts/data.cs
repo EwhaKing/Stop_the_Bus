f59cbe84ea798311d828a72a7c96d9ffaff6f990
imHyejinPark <53827279+imHyejinPark@users.noreply.github.com>
2021-07-10 17:26:08 +0900

    init

---
+++ Assets/Scripts/data.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class data : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static int speed; //버스 속도
+    public static int passenger; //승객 수
+
+    // 더 추가할 거 있으면 추가하셔요~
+
+}
---
+++ Assets/Scripts/mouse.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class mouse : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public WheelCollider eel;
+
+    private Vector3 mousePos;
+    private bool m = false; //마우스오버 여부 
+    
+    private float oldX;
+    private float oldY;
+    private float curX;
+    private float curY;
+    private float velocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m)
+        {
+            oldX = mousePos.x;
+            oldY = mousePos.y;
+
+            mousePos = Camera.main.ScreenToWorldPoint(
+            new Vector3(
+            Input.mousePosition.x,
+            Input.mousePosition.y,
+            -Camera.main.transform.position.z)
+            );
+
+            curX = mousePos.x;
+            curY = mousePos.y;
+
+            var v = Math.Pow(curX - oldX, 2) + Math.Pow(curY - oldY, 2);
+            velocity = (float)Math.Sqrt(v)/(float)Time.deltaTime; //속도
+            print(velocity);
+            eel.motorTorque = velocity;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        m = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        m = false;
+        print("Exit");
+    }
+
+    public float AlphaThreshold = 0.1f; //이미지 모양대로 버튼 인식하는 스크립트
+
+    void Start()
+    {
+        this.GetComponent<Image>().alphaHitTestMinimumThreshold = AlphaThreshold; //이미지 모양대로 버튼 인식하는 스크립트
+    }
+}
