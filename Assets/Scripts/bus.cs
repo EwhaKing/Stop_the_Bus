24cc14d0bca7bf80e95bc9d0886e0aadbfc21b4e
imHyejinPark <53827279+imHyejinPark@users.noreply.github.com>
2021-07-31 16:02:43 +0900

    가을맵

--- Assets/Scripts/bus.cs
+++ Assets/Scripts/bus.cs
@@ -5,5 +5,10 @@ using UnityEngine;
 public class bus : MonoBehaviour{
 
     //버스클래스를 만들어서 각 스테이지마다 버스의 가속도를 다르게 할 예정
+    public float accel;
+    public float speed;
+    // 버스마다 달라져야 하는 것? 
 
+
+ 
 }
