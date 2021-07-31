d6efd6ca2165dbb523154ea6a54138e158719c97
topnotchsh <dltngus00@ewhain.net>
2021-07-31 17:12:16 +0900

    tutorial scene & UI
    tutorial scene & UI

---
+++ Assets/Resources/button_test.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button_test : MonoBehaviour
+{
+    public void TestCall()
+    {
+        Debug.Log("Hello Unity");
+    }
+
+}
