104c31532fbc5ca5d168017bd38db3e853578e69
topnotchsh <dltngus00@ewhain.net>
2021-08-06 12:49:00 +0900

    update tutorial

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    float _Sec;
+    int _Min;
+    [SerializeField]
+    Text _TimerText;
+
+    private void Update()
+    {
+        realTimer();
+    }
+
+    void realTimer()
+    {
+        if (Goal.goal == false)
+        {
+            _Sec += Time.deltaTime;
+            _TimerText.text = string.Format("{0:D2}:{1:D2}", _Min, (int)_Sec);
+            if ((int)_Sec > 59)
+            {
+                _Sec = 0;
+                _Min++;
+            }
+        }
+    }
+}
