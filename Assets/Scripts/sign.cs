069ac7f2d9859c2475cdc1a63f34c73bd7e10deb
topnotchsh <dltngus00@ewhain.net>
2021-08-06 12:49:00 +0900

    update tutorial

---
+++ Assets/Scripts/sign.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sign : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
