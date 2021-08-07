0026d6292b9cd3533d02f042440bec40940d675f
Koohi45 <haein0405@ewhain.net>
2021-08-07 18:10:27 +0900

    가을 손님(fbx추가) & 빨간 점선 완료

---
+++ Assets/Scripts/Fall/FallFace.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FallFace : MonoBehaviour
+{
+    int comfort;
+    private Image face;
+
+    void Start()
+    {
+        face = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        comfort = FallComfort.GetComfort();
+
+        if (comfort <= 100 && comfort >= 80)
+            face.sprite = Resources.Load<Sprite>("기록_좋음");
+        else if (comfort < 80 && comfort >= 40)
+            face.sprite = Resources.Load<Sprite>("기록_보통");
+        else
+            face.sprite = Resources.Load<Sprite>("기록_나쁨");
+    }
+}
