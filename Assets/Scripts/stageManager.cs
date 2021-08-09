5057ac4c6bbc6a571d4dab693521d7696a2394f8
imHyejinPark <qkrgpwls1010@gmail.com>
2021-08-09 22:14:30 +0900

    stage 씬 연결

---
+++ Assets/Scripts/stageManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class stageManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void goStage(int n){
+        switch(n){
+            case 0:
+            SceneManager.LoadScene("spring");
+            break;
+            case 1:
+            SceneManager.LoadScene("Summer");
+            break;
+            case 2:
+            SceneManager.LoadScene("fall");
+            break;
+            case 3:
+            SceneManager.LoadScene("Winter");
+            break;
+        }
+    }
+}
