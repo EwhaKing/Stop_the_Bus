969e122c19c283397e1ea82fe1fccc65d253e92d
topnotchsh <dltngus00@ewhain.net>
2021-08-09 14:13:37 +0900

    블랙아이스 임시저장
    블랙아이스에 닿으면 키 무력화 구현/ 블랙아이스 도로 크기에 맞게 랜덤 스폰 아직 구현 못함

---
+++ Assets/Scripts/Winter/disableKey.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class disableKey : MonoBehaviour
+{
+    public static bool key = true;
+
+    void Update() {
+        if (!key){
+            if(Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.D))
+            {
+                disableKey.key = false;
+            }
+        }
+    }
+    void OnTriggerEnter(Collider col)
+        {
+            if(col.gameObject.tag == "Bus")
+            {
+                key = false;
+            }
+        }
+}
