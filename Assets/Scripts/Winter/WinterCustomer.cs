92d7256a930ee9f237ed5c9434f2543d22080b51
Koohi45 <haein0405@ewhain.net>
2021-08-14 00:30:25 +0900

    겨울 일시정지, 엔딩팝업 연동

--- Assets/Scripts/Winter/WinterCustomer.cs
+++ Assets/Scripts/Winter/WinterCustomer.cs
@@ -74,7 +74,7 @@ public class WinterCustomer : MonoBehaviour
 
     void Update()
     {
-        if (insign && bus.speed == 0 && eachtaken)
+        if (insign && WinterBus.speed == 0 && eachtaken)
         {
             soundCount += Time.deltaTime;
             if (soundCount >= 1f)
@@ -85,7 +85,7 @@ public class WinterCustomer : MonoBehaviour
             }
         }
 
-        if (insign && bus.speed == 0 && !eachtaken)
+        if (insign && WinterBus.speed == 0 && !eachtaken)
             if (TakenSound == 0)
             {
                 audioSource.clip = customerEnd;
@@ -109,7 +109,7 @@ public class WinterCustomer : MonoBehaviour
         if (wheel1 && wheel2 && wheel3 && wheel4)   // 네 바퀴가 모두 점선과 접촉해있을 때
         {
             insign = true;
-            if (bus.speed == 0)     // 버스 속도가 0이어야        
+            if (WinterBus.speed == 0)     // 버스 속도가 0이어야        
             {
                 if (timeCount > 0)
                     timeCount -= Time.deltaTime;
