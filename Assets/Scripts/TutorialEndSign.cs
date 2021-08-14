1363f0d3d93064e4cd3ef89bd3b4731846aa371f
Koohi45 <haein0405@ewhain.net>
2021-08-14 14:42:29 +0900

    튜토리얼 & 스테이지

---
+++ Assets/Scripts/TutorialEndSign.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialEndSign : MonoBehaviour
+{
+    Fade Panel;
+    string wheel;
+    bool wheel1, wheel2, wheel3, wheel4;
+    
+    void Start()
+    {
+        Panel = GameObject.Find("Canvas").transform.Find("FadeOut").GetComponent<Fade>();
+
+        wheel1 = false;
+        wheel2 = false;
+        wheel3 = false;
+        wheel4 = false;
+    }
+
+    void OnTriggerStay(Collider coll)
+    {
+        wheel = coll.gameObject.name;
+        if (wheel == "BUS_wheelLB")
+            wheel1 = true;
+        else if (wheel == "BUS_wheelLF")
+            wheel2 = true;
+        else if (wheel == "BUS_wheelRB")
+            wheel3 = true;
+        else if (wheel == "BUS_wheelRF")
+            wheel4 = true;
+
+        if (wheel1 && wheel2 && wheel3 && wheel4)
+        {
+            Panel.Panel.gameObject.SetActive(true);
+            Panel.ActiveFadeOut();
+            if(Panel.Panel.color.a == 1f)
+            {
+                Fade.change = true;
+                SceneManager.LoadScene("stageScene");
+            }
+        }
+    }
+
+}
--- Assets/Scripts/TutorialTotal.cs
+++ Assets/Scripts/TutorialTotal.cs
@@ -10,11 +10,20 @@ public class TutorialTotal : MonoBehaviour
     public static int SumOfCus;     //손님 합계
     private int count;              //손님 수 한 번만 더하기
 
+    GameObject Parent;
+    GameObject[] Child;
+
     void Start()
     {
         customerText = gameObject.GetComponent<TextMeshProUGUI>();
 
         obj = GameObject.Find("BusStopSign").GetComponent<TutorialCustomer>();
+
+        Parent = GameObject.FindWithTag("Bus");
+        Child = new GameObject[8];
+        for (int i = 0; i < Child.Length; i++)
+            Child[i] = Parent.transform.Find(string.Format("customerSit{0}", i + 1)).gameObject;
+
         SumOfCus = 0;
         count = 0;
     }
@@ -24,10 +33,31 @@ public class TutorialTotal : MonoBehaviour
         if (!obj.Taken() && count == 0)
         {
             SumOfCus += TutorialCustomer.Tutopassenger;
+            ActiveCustomer(SumOfCus);
             count++;
-            Debug.Log(string.Format("{0}번 버스 정류장 {1}명 태워서 총 {2}명", name, TutorialCustomer.Tutopassenger, SumOfCus));
         }
 
         customerText.text = SumOfCus.ToString();
+    }
+
+    void ActiveCustomer(int num)
+    {
+        if (num <= 8)
+        {
+            for (int i = 0; i < num; i++)
+            {
+                int rand = Random.Range(0, 8);
+                if (Child[rand].activeSelf)
+                    i--;
+                else
+                    Child[rand].SetActive(true);
+
+            }
+        }
+        else
+        {
+            for (int i = 0; i < 8; i++)
+                Child[i].SetActive(true);
+        }
     }
 }
