dbb37e781789a9ef3f924d3c97217f8eb0073fbb
yeong seo choi <2108melody@gmail.com>
2021-08-17 17:56:25 +0900

    로딩 넘어가는 거

---
+++ Assets/Scripts/toSeason.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class toSeason : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch (toLoading.ld)
+        {
+            case 0:
+                SceneManager.LoadScene("spring");
+                break;
+            case 1:
+                SceneManager.LoadScene("Summer");
+                break;
+            case 2:
+                SceneManager.LoadScene("fall");
+                break;
+            case 3:
+                SceneManager.LoadScene("Winter");
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
