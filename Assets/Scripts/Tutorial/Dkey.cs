4fae751d18c6d6825badf1acb90d13aa6edb5713
topnotchsh <dltngus00@ewhain.net>
2021-08-18 16:17:43 +0900

    튜토리얼 키 추가

---
+++ Assets/Scripts/Tutorial/Dkey.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dkey : MonoBehaviour
+{
+    public Image oldImage;
+    public Sprite TestSprite;
+    public Sprite OriginSprite;
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.D)){
+            ChangeImg();
+                }
+        if(Input.GetKeyUp(KeyCode.D)){
+            ChangeOrigin();
+        }
+    }
+    public void ChangeImg(){
+        oldImage.sprite = TestSprite;
+    }
+
+    public void ChangeOrigin(){
+        oldImage.sprite = OriginSprite;
+    }
+}
