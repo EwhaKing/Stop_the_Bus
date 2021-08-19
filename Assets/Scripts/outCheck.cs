1663e3fc1da704e3adf47127a5dab94de17e2c62
imHyejinPark <qkrgpwls1010@gmail.com>
2021-08-19 13:54:59 +0900

    효과음 교체
    마우스클릭 - 3번째
    빨간선 - 1번째

--- Assets/Scripts/outCheck.cs
+++ Assets/Scripts/outCheck.cs
@@ -6,14 +6,24 @@ using System;
 public class outCheck : MonoBehaviour{
     public GameObject popup;
     public GameObject panel;
+    public AudioSource outEff;
+    private bool flag;
 
-
+    void Start(){
+        flag = true;
+    }
     void Update(){
         if (bus.isOut){
             bus.speed = 0;
             //타이머 정지
+            if (flag){
+                Debug.Log("!!");
+                flag = false;
+                outEff.Play();
+            }
             popup.SetActive(true);
             panel.SetActive(true);
+
         }
     }
 
