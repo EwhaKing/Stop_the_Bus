14f01b77aa0d4420219c6e9c7be058f0bfa578fb
imHyejinPark <qkrgpwls1010@gmail.com>
2021-09-04 23:44:57 +0900

    로딩+계절씬 번역작업 완료
    시작화면 설정탭 변경

--- Assets/Scripts/infoText.cs
+++ Assets/Scripts/infoText.cs
@@ -15,16 +15,16 @@ public class infoText : MonoBehaviour
         switch (toLoading.ld)
         {
             case 0:
-                txt.text = "한 자리에 너무 오래 머무르면 승객들이 불만을 표시합니다.";
-                break;
+                txt.text = LocalizationManager.instance.GetLocalizedValue("txt_loading1");
+                break; 
             case 1:
-                txt.text = "물웅덩이에서는 원하지 않는 방향으로 미끄러질 수 있습니다.";
+                txt.text = LocalizationManager.instance.GetLocalizedValue("txt_loading2");
                 break;
             case 2:
-                txt.text = "아스팔트 포장도로는 길이 매끄러우니 속도 조절에 유의하세요.";
+                txt.text = LocalizationManager.instance.GetLocalizedValue("txt_loading3");
                 break;
             case 3:
-                txt.text = "푸른 빙판 위에서는 바퀴가 얼어붙을 수 있으니 조심하세요.";
+                txt.text = LocalizationManager.instance.GetLocalizedValue("txt_loading4");
                 break;
         }
     }
