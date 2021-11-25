7982836b5707e7da3675865e62c1b132e483f378
Koohi45 <haein0405@ewhain.net>
2021-11-26 00:57:15 +0900

    손님 수 도전과제

--- Assets/Scripts/Achievement/CustNumAchieve.cs
+++ Assets/Scripts/Achievement/CustNumAchieve.cs
@@ -14,21 +14,15 @@ public class CustNumAchieve : MonoBehaviour
 
         SteamUserStats.RequestCurrentStats();
 
-
         SteamUserStats.GetStat("PASSENGER_COUNT", out countPassenger);
         Debug.Log(countPassenger);
 
         //SteamUserStats.ResetAllStats(true);
 
-        if(countPassenger == 1) { return; }
-        SteamUserStats.SetAchievement("FIRST_PASSENGER");
-
-        if (countPassenger == 100) { return; }
-        SteamUserStats.SetAchievement("100TH_PASSENGER");
-        
-        if (countPassenger == 200) { return; }
-        SteamUserStats.SetAchievement("100TH_PASSENGER");
-                
+        if (countPassenger == 1) { SteamUserStats.SetAchievement("FIRST_PASSENGER"); }
+        if (countPassenger == 100) { SteamUserStats.SetAchievement("100TH_PASSENGER"); }
+        if (countPassenger == 200) { SteamUserStats.SetAchievement("200TH_PASSENGER"); }
+             
         SteamUserStats.StoreStats();
 
     }
