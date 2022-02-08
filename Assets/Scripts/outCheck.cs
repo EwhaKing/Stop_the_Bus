2196d2d07619584e37c4c89c0de8b22f1b040260
imHyejinPark <qkrgpwls1010@gmail.com>
2022-02-08 22:51:54 +0900

    팝업창 뜨면 일부 ui 가림
    손님 수 말풍선 + 마우스 커서 영역 겹쳐보여서 가림

--- Assets/Scripts/outCheck.cs
+++ Assets/Scripts/outCheck.cs
@@ -7,6 +7,9 @@ public class outCheck : MonoBehaviour{
     public GameObject popup;
     public GameObject panel;
     public AudioSource outEff;
+    public GameObject cursorMap;
+    public GameObject bubble; // 손님 수 말풍선
+    public Texture2D defaltCursor;
     private bool flag;
 
     void Start(){
@@ -15,14 +18,18 @@ public class outCheck : MonoBehaviour{
     void Update(){
         if (bus.isOut){
             bus.speed = 0;
+            Cursor.SetCursor (defaltCursor, Vector2.zero, CursorMode.Auto);
+
             //타이머 정지
             if (flag){
-                Debug.Log("!!");
                 flag = false;
                 outEff.Play();
             }
             popup.SetActive(true);
             panel.SetActive(true);
+
+            cursorMap.SetActive(false);
+            bubble.SetActive(false);
 
         }
     }
